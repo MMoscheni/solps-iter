1da181e2f0663648f16540abc255cc0c3ed00535
Matteo Moscheni <91872354+MMoscheni@users.noreply.github.com>
2022-03-04 09:24:29 +0000

    Add files via upload

--- solps_python_scripts/read_ft44.py
+++ solps_python_scripts/read_ft44.py
@@ -80,121 +80,116 @@ def read_ft44(file = None, save = None):
     neut['srcml']    = read_ft44_rfield(fid,ver,'srcml',[nx,ny,nmol])
     neut['edissml']  = read_ft44_rfield(fid,ver,'edissml',[nx,ny,nmol])
 
-    # ## Data on wall loading
-    # 
-    # # nlim, nsts, nstra
-    # dims  = fscanf(fid,'#d',3)
-    # nlim  = dims(1)
-    # nsts  = dims(2)
-    # nstra = dims(3)
-    # 
-    # wld['wldnek = zeros(nlim+nsts,nstra+1)
-    # wld['wldnep = zeros(nlim+nsts,nstra+1)
-    # wld['wldna  = zeros(nlim+nsts,natm,nstra+1)
-    # wld['ewlda  = zeros(nlim+nsts,natm,nstra+1)
-    # wld['wldnm  = zeros(nlim+nsts,nmol,nstra+1)
-    # wld['ewldm  = zeros(nlim+nsts,nmol,nstra+1)
-    # 
-    # wld['wldnek(:,1)  = read_ft44_rfield(fid,nlim+nsts)
-    # wld['wldnep(:,1)  = read_ft44_rfield(fid,nlim+nsts)
-    # wld['wldna(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,natm])
-    # wld['ewlda(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,natm])
-    # wld['wldnm(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    # wld['ewldm(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    # 
-    # wld['poly = read_ft44_rfield(fid,[4,nlim])
-    # 
-    # wld['wldra  = read_ft44_rfield(fid,[nlim+nsts,natm])
-    # wld['wldrm  = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    # 
-    # if (nstra > 1)
-    #     for i = 2:nstra+1
-    #         wld['wldnek(:,i)  = read_ft44_rfield(fid,nlim+nsts)
-    #         wld['wldnep(:,i)  = read_ft44_rfield(fid,nlim+nsts)
-    #         wld['wldna(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,natm])
-    #         wld['ewlda(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,natm])
-    #         wld['wldnm(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    #         wld['ewldm(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    #     end
-    # end
-    # 
-    # 
-    # wld['wldpp   = zeros(nlim+nsts,nfla,nstra+1)
-    # wld['wldpa   = zeros(nlim+nsts,natm,nstra+1)
-    # wld['wldpm   = zeros(nlim+nsts,nmol,nstra+1)
-    # wld['wldpeb  = zeros(nlim+nsts,nstra+1)
-    # wld['wldspt  = zeros(nlim+nsts,nstra+1)
-    # 
-    # wld['wldpp(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,nfla])
-    # wld['wldpa(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,natm])
-    # wld['wldpm(:,:,1) = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    # wld['wldpeb(:,1)  = read_ft44_rfield(fid,nlim+nsts)
-    # wld['wldspt(:,1)  = read_ft44_rfield(fid,nlim+nsts)
-    # 
-    # if (nstra > 1)
-    #     for i = 2:nstra+1
-    #         wld['wldpp(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,nfla])
-    #         wld['wldpa(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,natm])
-    #         wld['wldpm(:,:,i) = read_ft44_rfield(fid,[nlim+nsts,nmol])
-    #         wld['wldpeb(:,i)  = read_ft44_rfield(fid,nlim+nsts)
-    #         wld['wldspt(:,i)  = read_ft44_rfield(fid,nlim+nsts)
-    #     end
-    # end
-    # 
-    # wld['isrftype = read_ft44_ifield(fid,nlim+nsts)
+    ## Data on wall loading
+    
+    # nlim, nsts, nstra
+    dims  = fid.readline().split()
+    nlim  = int(dims[0])
+    nsts  = int(dims[1])
+    nstra = int(dims[2])
+    
+    # wld['wldnek'] = np.zeros((nlim+nsts,nstra+1))
+    # wld['wldnep'] = np.zeros((nlim+nsts,nstra+1))
+    # wld['wldna']  = np.zeros((nlim+nsts,natm,nstra+1))
+    # wld['ewlda']  = np.zeros((nlim+nsts,natm,nstra+1))
+    # wld['wldnm']  = np.zeros((nlim+nsts,nmol,nstra+1))
+    # wld['ewldm']  = np.zeros((nlim+nsts,nmol,nstra+1))
+    
+    # wld['wldnek'][:,0]  = read_ft44_rfield(fid,ver,'wldnek(0)',nlim+nsts)
+    # wld['wldnep'][:,0]  = read_ft44_rfield(fid,ver,'wldnep(0)',nlim+nsts)
+    # wld['wldna'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    # wld['ewlda'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    # wld['wldnm'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])
+    # wld['ewldm'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])
+    
+    # wld['poly'] = read_ft44_rfield(fid,ver,[4,nlim])
+    
+    # wld['wldra']  = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    # wld['wldrm']  = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])
+    
+    # if nstra > 1:
+    #     for i in range(1,nstra+1):
+    #         wld['wldnek'][:,i]  = read_ft44_rfield(fid,ver,nlim+nsts)
+    #         wld['wldnep'][:,i]  = read_ft44_rfield(fid,ver,nlim+nsts)
+    #         wld['wldna'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    #         wld['ewlda'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    #         wld['wldnm'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])
+    #         wld['ewldm'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])    
+    
+    # wld['wldpp']   = np.zeros((nlim+nsts,nfla,nstra+1))
+    # wld['wldpa']   = np.zeros((nlim+nsts,natm,nstra+1))
+    # wld['wldpm']   = np.zeros((nlim+nsts,nmol,nstra+1))
+    # wld['wldpeb']  = np.zeros((nlim+nsts,nstra+1))
+    # wld['wldspt']  = np.zeros((nlim+nsts,nstra+1))
+    
+    # wld['wldpp'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,nfla])
+    # wld['wldpa'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    # wld['wldpm'][:,:,0] = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])
+    # wld['wldpeb'][:,0]  = read_ft44_rfield(fid,ver,nlim+nsts)
+    # wld['wldspt'][:,0]  = read_ft44_rfield(fid,ver,nlim+nsts)
+    
+    # if nstra > 1:
+    #     for i in range(1,nstra+1):
+    #         wld['wldpp'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,nfla])
+    #         wld['wldpa'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,natm])
+    #         wld['wldpm'][:,:,i] = read_ft44_rfield(fid,ver,[nlim+nsts,nmol])
+    #         wld['wldpeb'][:,i]  = read_ft44_rfield(fid,ver,nlim+nsts)
+    #         wld['wldspt'][:,i]  = read_ft44_rfield(fid,ver,nlim+nsts)
+    
+    # wld['isrftype'] = read_ft44_ifield(fid,ver,nlim+nsts)
 
     # MMM decommented from here down to penultimate line
     if nlwrmsh == 0:
 
         raise ValueError("Not translated yet!")
         
-        # wld['wlarea']  = read_ft44_rfield(fid,nlim+nsts)
+        # wld['wlarea']  = read_ft44_rfield(fid,ver,nlim+nsts)
         
         # wld['wlabsrp'] = zeros(natm+nmol+nion+nfla,nlim+nsts)
         # line = fgetl(fid)
-        # wld['wlabsrp'](1:natm,:) = read_ft44_rfield(fid,[natm,nlim+nsts])
+        # wld['wlabsrp'](1:natm,:) = read_ft44_rfield(fid,ver,[natm,nlim+nsts])
         # line = fgetl(fid)
         # ns = natm
-        # wld['wlabsrp'](ns+1:ns+nmol,:) = read_ft44_rfield(fid,[nmol,nlim+nsts])
+        # wld['wlabsrp'](ns+1:ns+nmol,:) = read_ft44_rfield(fid,ver,[nmol,nlim+nsts])
         # line = fgetl(fid)
         # ns = natm+nmol
-        # wld['wlabsrp'](ns+1:ns+nion,:) = read_ft44_rfield(fid,[nion,nlim+nsts])
+        # wld['wlabsrp'](ns+1:ns+nion,:) = read_ft44_rfield(fid,ver,[nion,nlim+nsts])
         # line = fgetl(fid)
         # ns = natm+nmol+nion
-        # wld['wlabsrp'](ns+1:ns+nfla,:) = read_ft44_rfield(fid,[nfla,nlim+nsts])
+        # wld['wlabsrp'](ns+1:ns+nfla,:) = read_ft44_rfield(fid,ver,[nfla,nlim+nsts])
         
-        # neut['eneutrad'] = read_ft44_rfield(fid,[nx,ny,natm])
+        # neut['eneutrad'] = read_ft44_rfield(fid,ver,[nx,ny,natm])
         
-        # wld['eirdiag_nds_ind']   = read_ft44_ifield(fid,nsts+1)
-        # wld['eirdiag_nds_typ']   = read_ft44_ifield(fid,nsts)
-        # wld['eirdiag_nds_srf']   = read_ft44_ifield(fid,nsts)
-        # wld['eirdiag_nds_start'] = read_ft44_ifield(fid,nsts)
-        # wld['eirdiag_nds_end']   = read_ft44_ifield(fid,nsts)
+        # wld['eirdiag_nds_ind']   = read_ft44_ifield(fid,ver,nsts+1)
+        # wld['eirdiag_nds_typ']   = read_ft44_ifield(fid,ver,nsts)
+        # wld['eirdiag_nds_srf']   = read_ft44_ifield(fid,ver,nsts)
+        # wld['eirdiag_nds_start'] = read_ft44_ifield(fid,ver,nsts)
+        # wld['eirdiag_nds_end']   = read_ft44_ifield(fid,ver,nsts)
         
         # ncl = wld['eirdiag_nds_ind'](nsts+1)
         
-        # wld['sarea_res']  = read_ft44_rfield(fid,ncl)
-        # wld['wldna_res']  = read_ft44_rfield(fid,[natm,ncl])
-        # wld['wldnm_res']  = read_ft44_rfield(fid,[nmol,ncl])
-        # wld['ewldna_res'] = read_ft44_rfield(fid,[natm,ncl])
-        # wld['ewldnm_res'] = read_ft44_rfield(fid,[nmol,ncl])
-        # wld['ewldea_res'] = read_ft44_rfield(fid,[natm,ncl])
-        # wld['ewldem_res'] = read_ft44_rfield(fid,[nmol,ncl])
-        # wld['ewldrp_res'] = read_ft44_rfield(fid,ncl)
-        # wld['ewldmr_res'] = read_ft44_rfield(fid,[nmol,ncl])
+        # wld['sarea_res']  = read_ft44_rfield(fid,ver,ncl)
+        # wld['wldna_res']  = read_ft44_rfield(fid,ver,[natm,ncl])
+        # wld['wldnm_res']  = read_ft44_rfield(fid,ver,[nmol,ncl])
+        # wld['ewldna_res'] = read_ft44_rfield(fid,ver,[natm,ncl])
+        # wld['ewldnm_res'] = read_ft44_rfield(fid,ver,[nmol,ncl])
+        # wld['ewldea_res'] = read_ft44_rfield(fid,ver,[natm,ncl])
+        # wld['ewldem_res'] = read_ft44_rfield(fid,ver,[nmol,ncl])
+        # wld['ewldrp_res'] = read_ft44_rfield(fid,ver,ncl)
+        # wld['ewldmr_res'] = read_ft44_rfield(fid,ver,[nmol,ncl])
         
-        # neut['PDENA_INT']    = read_ft44_rfield(fid,[natm,nstrata])
-        # neut['PDENM_INT']    = read_ft44_rfield(fid,[nmol,nstrata])
-        # neut['PDENI_INT']    = read_ft44_rfield(fid,[nion,nstrata])
-        # neut['PDENA_INT_B2'] = read_ft44_rfield(fid,[natm,nstrata])
-        # neut['PDENM_INT_B2'] = read_ft44_rfield(fid,[nmol,nstrata])
-        # neut['PDENI_INT_B2'] = read_ft44_rfield(fid,[nion,nstrata])
-        # neut['EDENA_INT']    = read_ft44_rfield(fid,[natm,nstrata])
-        # neut['EDENM_INT']    = read_ft44_rfield(fid,[nmol,nstrata])
-        # neut['EDENI_INT']    = read_ft44_rfield(fid,[nion,nstrata])
-        # neut['EDENA_INT_B2'] = read_ft44_rfield(fid,[natm,nstrata])
-        # neut['EDENM_INT_B2'] = read_ft44_rfield(fid,[nmol,nstrata])
-        # neut['EDENI_INT_B2'] = read_ft44_rfield(fid,[nion,nstrata])
+        # neut['PDENA_INT']    = read_ft44_rfield(fid,ver,[natm,nstrata])
+        # neut['PDENM_INT']    = read_ft44_rfield(fid,ver,[nmol,nstrata])
+        # neut['PDENI_INT']    = read_ft44_rfield(fid,ver,[nion,nstrata])
+        # neut['PDENA_INT_B2'] = read_ft44_rfield(fid,ver,[natm,nstrata])
+        # neut['PDENM_INT_B2'] = read_ft44_rfield(fid,ver,[nmol,nstrata])
+        # neut['PDENI_INT_B2'] = read_ft44_rfield(fid,ver,[nion,nstrata])
+        # neut['EDENA_INT']    = read_ft44_rfield(fid,ver,[natm,nstrata])
+        # neut['EDENM_INT']    = read_ft44_rfield(fid,ver,[nmol,nstrata])
+        # neut['EDENI_INT']    = read_ft44_rfield(fid,ver,[nion,nstrata])
+        # neut['EDENA_INT_B2'] = read_ft44_rfield(fid,ver,[natm,nstrata])
+        # neut['EDENM_INT_B2'] = read_ft44_rfield(fid,ver,[nmol,nstrata])
+        # neut['EDENI_INT_B2'] = read_ft44_rfield(fid,ver,[nion,nstrata])
     else:
         # MMM
         neut['eneutrad'] = read_ft44_rfield(fid,ver,'eneutrad',[nx,ny,natm])
--- solps_python_scripts/read_ft44_rfield.py
+++ solps_python_scripts/read_ft44_rfield.py
@@ -17,35 +17,40 @@ def read_ft44_rfield(fid = None, ver = None, fieldname = None, dims = None):
 
     # Version 20160829: field label and size are specified in fort.44
     # Do consistency check on the data
+
     if ver >= 20160829:
         # Search the file until identifier 'fieldname' is found
-        line = fid.readline().split()
-        try:
-            while line[3] != fieldname:
-                line = fid.readline().split()
-                if line == "":
-                    raise ValueError("EOF reached without finding" + fieldname + ".")
-        except:
-            pass
+        line = fid.readline()
+        print(fieldname)
+        while line.find(fieldname) == -1:
+            line = fid.readline()
+            if line == "":
+                raise ValueError("EOF reached without finding" + fieldname + ".")
+
+        line = line.split()
         
         # Consistency check: number of elements specified in the file should equal
         # prod(dims)
+        
         numin = int(line[6])
         if numin != np.array(dims).prod():
             raise ValueError('read_ft44_rfield: inconsistent number of input elements.')
 
-    # Read the data
-    iin = 0
-    field = np.zeros((numin))
-    while iin < numin and line != "":
-        line = fid.readline().split()
-        iil = 0
-        while iil < len(line):
-            field[iin + iil] = line[iil]
-            iil += 1
-        iin += len(line)
+        # Read the data
+        iin = 0
+        field = np.zeros((numin))
+        while iin < numin and line != "":
+            line = fid.readline().split()
+            iil = 0
+            while iil < len(line):
+                field[iin + iil] = line[iil]
+                iil += 1
+            iin += len(line)
 
-    if len(dims) > 1:
-        field = np.reshape(field, tuple(dims), order = "F")
+        if len(dims) > 1:
+            field = np.reshape(field, tuple(dims), order = "F")
+
+    else:
+        raise ValueError("fort.44 version NOT regonised!")
     
     return field
